32885f5f68f687d20d2a9b99d160caba0398d7f6
Leite <slle@gft.com>
2022-07-14 14:51:55 -0300

    first automation script update

--- src/main/java/utils/testBase.java
+++ src/main/java/utils/testBase.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,16 +18,21 @@ public class testBase {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\resources\\global.properties");
         Properties prop = new Properties();
         prop.load(fis);
-        String url = prop.getProperty("QAUrl");
+        String url = prop.getProperty("QAUrl"); // faz Get da URL
+        String browser_properties = prop.getProperty("browser"); // faz Get da informação no arquivo globais enfatizando o browser
+        String browser_maven = System.getProperty("browser"); // codigo para executar via Maven em linha de comando
+
+        String browser = browser_maven!=null ? browser_maven : browser_properties;
 
         if (driver == null){
 
-            if (prop.getProperty("browser").equalsIgnoreCase("chrome")){
-                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+            if (browser.equalsIgnoreCase("chrome")){
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\browsers\\chromedriver.exe");
                 driver = new ChromeDriver();
             }
-            if (prop.getProperty("browser").equalsIgnoreCase("firefox")){
-//                Inserir comando para executar o Firefox
+            if (browser.equalsIgnoreCase("firefox")){
+                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\browsers\\geckodriver.exe");
+                driver = new FirefoxDriver();
             }
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
             driver.get(url);
